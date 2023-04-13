b43821608bbd3e996b3a246e9ca8d4fab770ed84
hiteshhedwig <hitesh.kumar61@yahoo.com>
2023-04-13 13:49:01 +0530

    add bubbles

--- assets/js/particle.js
+++ assets/js/particle.js
@@ -1,99 +1,106 @@
 /* ---- particles.js config ---- */
+let direction = 'top'
+setInterval(() => {
+  direction = direction === 'top' ? 'bottom' : 'top';
+}, 1000);
 
-particlesJS("particles-js", {
-  particles: {
-    number: {
-      value: 140,
-      density: {
-        enable: true,
-        value_area: 800,
-      },
-    },
-    color: {
-      value: ["#2EB67D", "#ECB22E", "#E01E5B", "#36C5F0"],
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#fff",
-      },
-      polygon: {
-        nb_sides: 5,
-      },
-      image: {
-        src: "https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png",
-        width: 100,
-        height: 100,
-      },
-    },
+particlesJS("particles-js", {"particles":{"number":{"value":400,"density":{"enable":true,"value_area":800}},"color":{"value":"#fff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":10,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":500,"color":"#ffffff","opacity":0.4,"width":2},"move":{"enable":true,"speed":3,"direction": direction,"random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":0.5}},"bubble":{"distance":400,"size":4,"duration":0.3,"opacity":1,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
-    opacity: {
-      value: 1,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false,
-      },
-    },
-    size: {
-      value: 8,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 10,
-        size_min: 10,
-        sync: false,
-      },
-    },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#808080",
-      opacity: 0.4,
-      width: 1,
-    },
-    move: {
-      enable: true,
-      speed: 5,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200,
-      },
-    },
-  },
-  interactivity: {
-    detect_on: "window",
-    events: {
-      onhover: {
-        enable: true,
-        mode: "repulse",
-      },
-      onclick: {
-        enable: true,
-        mode: "push",
-      },
-    },
-    modes: {
-      repulse: {
-        distance: 70,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-    },
-  },
-  retina_detect: true,
-});
+// particlesJS("particles-js", {
+//   particles: {
+//     number: {
+//       value: 140,
+//       density: {
+//         enable: true,
+//         value_area: 800,
+//       },
+//     },
+//     color: {
+//       value: ["#2EB67D", "#ECB22E", "#E01E5B", "#36C5F0"],
+//     },
+//     shape: {
+//       type: "circle",
+//       stroke: {
+//         width: 0,
+//         color: "#fff",
+//       },
+//       polygon: {
+//         nb_sides: 5,
+//       },
+//       image: {
+//         src: "https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png",
+//         width: 100,
+//         height: 100,
+//       },
+//     },
+
+//     opacity: {
+//       value: 1,
+//       random: false,
+//       anim: {
+//         enable: false,
+//         speed: 1,
+//         opacity_min: 0.1,
+//         sync: false,
+//       },
+//     },
+//     size: {
+//       value: 8,
+//       random: true,
+//       anim: {
+//         enable: false,
+//         speed: 10,
+//         size_min: 10,
+//         sync: false,
+//       },
+//     },
+//     line_linked: {
+//       enable: true,
+//       distance: 150,
+//       color: "#808080",
+//       opacity: 0.4,
+//       width: 1,
+//     },
+//     move: {
+//       enable: true,
+//       speed: 5,
+//       direction: "none",
+//       random: false,
+//       straight: false,
+//       out_mode: "out",
+//       bounce: false,
+//       attract: {
+//         enable: false,
+//         rotateX: 600,
+//         rotateY: 1200,
+//       },
+//     },
+//   },
+//   interactivity: {
+//     detect_on: "window",
+//     events: {
+//       onhover: {
+//         enable: true,
+//         mode: "repulse",
+//       },
+//       onclick: {
+//         enable: true,
+//         mode: "push",
+//       },
+//     },
+//     modes: {
+//       repulse: {
+//         distance: 70,
+//         duration: 0.4,
+//       },
+//       push: {
+//         particles_nb: 4,
+//       },
+//     },
+//   },
+//   retina_detect: true,
+// });
+
 const allElements = document.querySelectorAll(".animated-text");
 
 // It checks if there is at least one element
